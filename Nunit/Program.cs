76ac7cc9aade5f1b923591815aef423f1a81dd2a
Krzysztof Simon <simon3ti@gmail.com>
2014-04-08 13:58:38 +0200

    Dodanie klasy figura

---
+++ Nunit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProstokatTest test = new ProstokatTest();
+            test.PoleTest();
+            test.ObwodTest();
+        }
+    }
+}
