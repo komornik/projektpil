033432b572249c6046de15e6a1d25cdd877925be
Krystian Wieczorek <krystianwieczorek@o2.pl>
2014-04-24 19:28:19 +0200

    Dodanie klasy figura

---
+++ Nunit/Figura.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Nunit
+{
+    abstract class Figura
+    {
+        private double szerokosc;
+
+        public double Szerokosc
+        {
+            get { return szerokosc; }
+            set { szerokosc = value; }
+        }
+
+        private double wysokosc;
+        public double Wysokosc
+        {
+            get { return wysokosc; }
+            set { wysokosc = value; }
+        }
+
+        abstract public double pole();
+
+        abstract public double obwod();
+
+
+    }
+}
