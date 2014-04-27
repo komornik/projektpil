c404972116dbcb855c87ddda9976a09bdb732501
Krzysztof Simon <simon3ti@gmail.com>
2014-04-27 12:12:08 +0200

    a

---
+++ Nunit/ClassLibrary2/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Nunit;
+
+
+namespace ClassLibrary2
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void test()
+        {
+            Krzysztof k = new Krzysztof();
+            Assert.AreEqual(1, k.kk());
+        }
+    }
+}
