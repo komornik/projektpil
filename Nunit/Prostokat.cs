258dcb262c91f614ff2307586fa1d25fbefd9f4c
Krystian Wieczorek <krystianwieczorek@o2.pl>
2014-04-27 10:58:26 +0200

    Poprawa walidacji danych w klasie Prostokąt

--- Nunit/Prostokat.cs
+++ Nunit/Prostokat.cs
@@ -4,13 +4,21 @@ using System.Text;
 
 namespace Nunit
 {
-    class Prostokat : Figura
+    public class Prostokat : Figura
     {
+        /// <summary>
+        /// Pusty konstruktor klasy prostokąt
+        /// </summary>
         public Prostokat()
         {
             ;
         }
 
+        /// <summary>
+        /// Konstruktor dwu argumentowy
+        /// </summary>
+        /// <param name="wysokosc">Bok B prostokąta (wysokość)</param>
+        /// <param name="szerokosc">Bok A prostokąta (szerokość)</param>
         public Prostokat(double wysokosc, double szerokosc)
         {
             this.Szerokosc = szerokosc;
@@ -25,12 +33,31 @@ namespace Nunit
         /// <returns>pole prostokata</returns>
         public override double pole()
         {
-            return this.Szerokosc * this.Wysokosc;
+            if (this.Szerokosc > 0 && this.Wysokosc > 0)
+            {
+                return this.Szerokosc * this.Wysokosc;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+            throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Metoda obliczająca obwód prostokąta
+        /// </summary>
+        /// <returns>Obwód prostokąt</returns>
         public override double obwod()
         {
-            return (2 * this.Szerokosc) + (2 * this.Wysokosc);
+            if (this.Szerokosc > 0 && this.Wysokosc > 0)
+            {
+                return (2 * this.Szerokosc) + (2 * this.Wysokosc);
+            }else
+            {
+                throw new ArgumentException();
+            }
+            throw new NotImplementedException();
         }
     }
 }
